472c980fa8478a62301f0b20c5ad59a87cd8ca72
Ignacio Serrano <ignacio.serranorincon@gmail.com>
2023-06-23 10:32:53 +0000

    task

---
+++ Lab/AccessSpecifiersDemo/src/com/bean/Manager.java
@@ -0,0 +1,13 @@
+package com.bean;
+
+public class Manager extends Employee{
+
+	public void managerInfo() {
+		//System.out.println(" Id "+id);		// private : scope same class. 
+		System.out.println(" Name "+name);
+		System.out.println(" Salary "+salary);
+		System.out.println(" Designation "+designation);
+		empInfo();
+		
+	}
+}
